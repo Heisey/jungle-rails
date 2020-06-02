0bcb518b5733518820720ea80acd9a31afb8cf55
heisey <jheisler01@gmail.com>
2020-06-02 22:38:57 +0000

    finished task

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,13 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :destroy]
   end
+
+  get '/signup' => 'user#new'
+  post '/users' => 'user#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
